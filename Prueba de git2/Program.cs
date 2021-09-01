64d41b7c9052a798be509b50aa56af2e1011a875
EmmanuelJulio <47417202+EmmanuelJulio@users.noreply.github.com>
2021-09-01 13:08:25 -0300

    Primer comit

---
+++ Prueba de git2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Prueba_de_git2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecione el miembro de los borbotones");
+            Console.WriteLine("1 Emmanuel Julio");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            switch (opcion)
+                {
+                case 1:
+                    Console.WriteLine("Emmanuel julio edad 29 años tell 1138427868");
+                    break;
+                }
+        }
+    }
+}
